4b112b41e6ea814261af0cc4d20be0b50d0d7f4a
ashnewport <chickensoup64@gmail.com>
2018-10-17 09:22:01 +1100

    Restore angular-firestarter from git cache

---
+++ angular/angular-firestarter/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDMVqptsdF3Y2dTUjJg1yRVFCSapWhvvww",
+    authDomain: "firestarterprod-c9d76.firebaseapp.com",
+    databaseURL: "https://firestarterprod-c9d76.firebaseio.com",
+    projectId: "firestarterprod-c9d76",
+    storageBucket: "firestarterprod-c9d76.appspot.com",
+    messagingSenderId: "363551214043"
+  }
+};
