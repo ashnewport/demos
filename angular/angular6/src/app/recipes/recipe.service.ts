583b5c70b060c1c57dc86b99b49fdd5ffbed69a1
ashnewport <chickensoup64@gmail.com>
2018-10-18 09:11:27 +1100

    serivce to replace cross-component property binding

--- angular/angular6/src/app/recipes/recipe.service.ts
+++ angular/angular6/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  public recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://picsum.photos/g/300'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://picsum.photos/g/300')
