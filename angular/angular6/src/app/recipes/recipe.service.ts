ea5680cb2bdb3236808c9aa197b4ea2355518842
ashnewport <chickensoup64@gmail.com>
2018-10-19 11:19:22 +1100

    configure route parameters

--- angular/angular6/src/app/recipes/recipe.service.ts
+++ angular/angular6/src/app/recipes/recipe.service.ts
@@ -32,6 +32,10 @@ export class RecipeService {
     return this.recipes.slice(); // return new array instead of reference to existing
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
