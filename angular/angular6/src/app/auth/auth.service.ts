e6d15536e352681086b001c0caf3f387f57a539b
ashnewport <chickensoup64@gmail.com>
2018-11-08 09:23:32 +1100

    signing up new users

--- angular/angular6/src/app/auth/auth.service.ts
+++ angular/angular6/src/app/auth/auth.service.ts
@@ -1,5 +1,14 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthService {
   signUpUser(email: string, password: string) {
-
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(
+        error => console.log('createUserWithEmailAndPassword', error)
+      );
   }
 }
