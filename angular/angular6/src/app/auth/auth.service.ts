3d9c0080a60474c57bac29a4e143e4da72f27a23
ashnewport <chickensoup64@gmail.com>
2018-11-08 14:09:00 +1100

    route protection and redirect

--- angular/angular6/src/app/auth/auth.service.ts
+++ angular/angular6/src/app/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   token: string;
+
+  constructor(private router: Router) {}
 
   signUpUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -18,6 +21,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken()
           .then(
             (token: string) => this.token = token
